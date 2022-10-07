cfb266d30331fe6324f9f86a919ddcb648834db1
anuredapple2014 <anurag@redappletech.com>
2022-10-07 18:52:23 +0530

    commit 2

---
+++ src/Layout/Header.js
@@ -0,0 +1,21 @@
+//import { StyleSheet, Text, View } from 'react-native'
+import React, { Fragment } from "react";
+import mealsImage from "../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table is full of delicious food " />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
